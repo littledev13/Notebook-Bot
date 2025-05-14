87adbedcf979437439e50f3fc236b761c27d78bd
littledev13 <irvandn13@gmail.com>
2025-05-14 16:24:46 +0700

    Signature Done

--- bitget.js
+++ bitget.js
@@ -1,11 +1,16 @@
-import crypto from "crypto-js";
+import crypto from "crypto";
 import dotenv from "dotenv";
 import axios from "axios";
-import https from "https";
+import { Base64 } from "js-base64";
 
 //
 dotenv.config();
 
+// const secret = "abcdefg";
+// const hash = createHmac("sha256", secret)
+//   .update("776a88db6706e2eb7711a65bc40a6456d1078f2073b36763976cd190b88119e3")
+//   .digest("hex");
+// console.log(hash);
 //
 
 // 1. Buat pesan
@@ -15,18 +20,23 @@ dotenv.config();
 // const signature = crypto.HmacSHA256(message, secretKey).toString();
 
 async function getBalance() {
-  const ts = Date.now();
+  const ts = Math.round(new Date());
   const BASE_URL = "https://api.bitget.com";
-  const API_KEY = process.env.publicKey;
-  const SECRET_KEY = process.env.privKey;
+  const API_KEY = "bg_e9168e5a18f0df7b9138984543b4df4e";
+  const SECRET_KEY =
+    "776a88db6706e2eb7711a65bc40a6456d1078f2073b36763976cd190b88119e3";
   const method = "GET";
   const path = "/api/v2/account/all-account-balance";
   const body = ""; // Kosong untuk GET
   const message = ts + method.toUpperCase() + path;
-  const httpsAgent = new https.Agent({ rejectUnauthorized: false });
 
   // Generate signature
-  const signature = crypto.HmacSHA256(message, SECRET_KEY).toString();
+
+  // const signature = crypto.HmacSHA256(message, SECRET_KEY).toString();
+  const signature = crypto
+    .createHmac("sha256", SECRET_KEY)
+    .update(message)
+    .digest("Base64");
 
   // Header
   const headers = {
@@ -39,13 +49,16 @@ async function getBalance() {
   };
 
   // Request
+  console.log(signature);
+
   try {
-    const response = await axios.get(BASE_URL + path, { headers, httpsAgent });
+    const response = await axios.get(BASE_URL + path + body, {
+      headers,
+    });
     console.log(response.data);
-    console.log("sukses");
+    // console.log("sukses");
   } catch (error) {
     console.error("Response Error:", error.response.data);
-    console.log("error nns");
 
     if (error.response) {
       console.error("Response Error:", error.response.data);
@@ -56,14 +69,3 @@ async function getBalance() {
 }
 
 getBalance();
-
-// const ts = Date.now();
-// const BASE_URL = "https://api.bitget.com";
-// const API_KEY = process.env.publicKey;
-// const SECRET_KEY = process.env.privKey;
-// const method = "GET";
-// const path = "/api/v2/account/all-account-balance";
-// const body = ""; // Kosong untuk GET
-// const message = ts + method + path;
-// const signature = crypto.HmacSHA256(message, SECRET_KEY).toString();
-// console.log(signature);
