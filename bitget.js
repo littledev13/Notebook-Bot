1444ed564ca5c1b6c7f263b5bc66091b387a00a9
littledev13 <irvandn13@gmail.com>
2025-05-14 20:54:22 +0700

    New

--- bitget.js
+++ bitget.js
@@ -1,34 +1,15 @@
 import crypto from "crypto";
-import dotenv from "dotenv";
 import axios from "axios";
-import { Base64 } from "js-base64";
 
-//
-dotenv.config();
+const ts = Date.now();
+const BASE_URL = "https://api.bitget.com";
+const API_KEY = "bg_e9168e5a18f0df7b9138984543b4df4e";
+const SECRET_KEY =
+  "776a88db6706e2eb7711a65bc40a6456d1078f2073b36763976cd190b88119e3";
 
-// const secret = "abcdefg";
-// const hash = createHmac("sha256", secret)
-//   .update("776a88db6706e2eb7711a65bc40a6456d1078f2073b36763976cd190b88119e3")
-//   .digest("hex");
-// console.log(hash);
-//
-
-// 1. Buat pesan
-// const message = ts + method + path + body;
-
-// 2. Generate signature
-// const signature = crypto.HmacSHA256(message, secretKey).toString();
-
-async function getBalance() {
-  const ts = Math.round(new Date());
-  const BASE_URL = "https://api.bitget.com";
-  const API_KEY = "bg_e9168e5a18f0df7b9138984543b4df4e";
-  const SECRET_KEY =
-    "776a88db6706e2eb7711a65bc40a6456d1078f2073b36763976cd190b88119e3";
-  const method = "GET";
-  const path = "/api/v2/account/all-account-balance";
+const getData = async (path, query = "") => {
   const body = ""; // Kosong untuk GET
-  const message = ts + method.toUpperCase() + path;
+  const message = ts + "GET" + path + query;
 
   // Generate signature
 
@@ -38,34 +19,43 @@ async function getBalance() {
     .update(message)
     .digest("Base64");
 
+  console.log(message);
+  console.log(signature);
   // Header
   const headers = {
     "ACCESS-KEY": API_KEY,
-    "ACCESS-SIGN": signature,
     "ACCESS-PASSPHRASE": "irvan13699",
+    "ACCESS-SIGN": signature,
     "ACCESS-TIMESTAMP": ts,
     locale: "en-US",
     "Content-Type": "application/json",
   };
 
-  // Request
-  console.log(signature);
-
   try {
-    const response = await axios.get(BASE_URL + path + body, {
+    const response = await axios.get(BASE_URL + path, {
       headers,
     });
-    console.log(response.data);
-    // console.log("sukses");
+    console.log("sukses");
+    return response;
   } catch (error) {
-    console.error("Response Error:", error.response.data);
+    console.log("error");
 
+    // console.log("Response Error:", error);
     if (error.response) {
-      console.error("Response Error:", error.response.data);
+      console.log("Response Error:", error.response.data.msg);
     } else {
-      console.error("Network/System Error:", error.message);
+      console.log("Network/System Error:", error.message);
     }
   }
-}
+};
 
-getBalance();
+const path = "/api/v2/mix/position/all-position?";
+const month = ts - 7 * 24 * 60 * 60 * 1000;
+const params = { startTime: month, endTime: ts };
+// const query = `startTime=${params.startTime}&endTime=${params.endTime}`;
+const query = "productType=USDT-FUTURES";
+
+(async () => {
+  const data = await getData(path, query);
+  // console.log(data); // Tampilkan data setelah mendapatkan hasil
+})();
