94ad0ca9cc2a2d362c7e460aeeef088ac6f866c2
littledev13 <irvandn13@gmail.com>
2025-05-15 17:28:14 +0700

    path api

--- bitget.js
+++ bitget.js
@@ -5,67 +5,44 @@ const ts = Date.now();
 const BASE_URL = "https://api.bitget.com";
 const API_KEY = "bg_e9168e5a18f0df7b9138984543b4df4e";
 const SECRET_KEY =
-  "776a88db6706e2eb7711a65bc40a6456d1078f2073b36763976cd190b88119e3";
+    "776a88db6706e2eb7711a65bc40a6456d1078f2073b36763976cd190b88119e3";
 
 const getData = async (path, query = "") => {
-  const message = ts + "GET" + path + (query ? "?" + query : "");
+    const message = ts + "GET" + path + (query ? "?" + query : "");
 
-  const signature = crypto
-    .createHmac("sha256", SECRET_KEY)
-    .update(message)
-    .digest("Base64");
-  const headers = {
-    "ACCESS-KEY": API_KEY,
-    "ACCESS-PASSPHRASE": "irvan13699",
-    "ACCESS-SIGN": signature,
-    "ACCESS-TIMESTAMP": ts,
-    locale: "en-US",
-    "Content-Type": "application/json",
-  };
-  try {
-    const response = await axios.get(
-      BASE_URL + path + (query ? "?" + query : ""),
-      {
-        headers,
-      }
-    );
-    return response;
-  } catch (error) {
-    if (error.response) {
-      console.log("Response Error:", error.response.data.msg);
-    } else {
-      console.log("Network/System Error:", error.message);
+    const signature = crypto
+        .createHmac("sha256", SECRET_KEY)
+        .update(message)
+        .digest("Base64");
+    const headers = {
+        "ACCESS-KEY": API_KEY,
+        "ACCESS-PASSPHRASE": "irvan13699",
+        "ACCESS-SIGN": signature,
+        "ACCESS-TIMESTAMP": ts,
+        locale: "en-US",
+        "Content-Type": "application/json"
+    };
+    try {
+        const response = await axios.get(
+            BASE_URL + path + (query ? "?" + query : ""),
+            {
+                headers
+            }
+        );
+        return {
+            msg: response.data.msg,
+            data: response.data.data
+        };
+    } catch (error) {
+        if (error.response) {
+            console.log("Response Error:", error.response.data.msg);
+        } else {
+            console.log("Network/System Error:", error.message);
+        }
     }
-  }
 };
-const tgl = (hari) => {
-  return ts - hari * 24 * 60 * 60 * 1000;
-};
-const balance = () => {
-  const path = "/api/v2/account/all-account-balance";
-  return getData(path)
-    .then((data) => {
-      return {
-        msg: data.data.msg,
-        data: data.data.data,
-      };
-    })
-    .catch((e) => {
-      return {
-        msg: "Error fetching balance",
-        error: e.message || "Unknown error",
-      };
-    });
+const tgl = hari => {
+    return ts - hari * 24 * 60 * 60 * 1000;
 };
 
-const path = "/api/v2/account/all-account-balance";
-const params = { startTime: tgl(3), endTime: ts };
-const query = "productType=USDT-FUTURES";
-
-(async () => {
-  ``;
-  const data = await getData(path);
-  // console.log(data.data);
-})();
-
-export { balance };
+export { getData, tgl };
---
+++ pathApi.js
@@ -0,0 +1,6 @@
+//? Balance
+// todo No Need params
+const balance = "/api/v2/account/all-account-balance";
+
+// Spot Order
+const spot =""
