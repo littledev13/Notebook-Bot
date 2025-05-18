45b950924eb3ac44531e18b98e9e969ac6ee5142
littledev13 <irvandn13@gmail.com>
2025-05-18 17:35:58 +0700

    transform to express

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 // TODO Import
 import TelegramBot from "node-telegram-bot-api";
+import express from "express";
+const app = express();
 import { get, gets, postTrade } from "./firebase.js";
 import { onInputText } from "./controller.js";
 import { message, callBack } from "./callback.js";
@@ -24,23 +26,29 @@ bot.on("callback_query", (callbackQuery) => {
   callBack(callbackQuery, bot);
 });
 
-console.log("Bot-tele Running...");
-//!! 1 atau 3 String dalam object
-const data2 = await gets(["Balance", "Deposit", "history"]);
+app.get("/", (req, res) => res.send("Express on Vercel"));
 
-// postTrade({ Total: 40, Type: "Deposit" }, ["Balance", "Deposit", "history"]);
-const timestamp = 1736121600; // Timestamp UNIX dalam detik
-const dateUTC = new Date(timestamp * 1000); // Konversi ke milidetik
+app.listen(3000, (pre) => console.log("Server Tele running... "));
 
-// Format waktu ke zona waktu GMT+7 (Waktu Indonesia Barat)
-const options = {
-  timeZone: "Asia/Jakarta", // GMT+7
-  year: "numeric",
-  month: "long",
-  day: "numeric",
-  hour: "2-digit",
-  minute: "2-digit",
-  second: "2-digit",
-};
+// console.log("Bot-tele Running...");
+// //!! 1 atau 3 String dalam object
+// const data2 = await gets(["Balance", "Deposit", "history"]);
 
-const dateInGMT7 = new Intl.DateTimeFormat("id-ID", options).format(dateUTC);
+// // postTrade({ Total: 40, Type: "Deposit" }, ["Balance", "Deposit", "history"]);
+// const timestamp = 1736121600; // Timestamp UNIX dalam detik
+// const dateUTC = new Date(timestamp * 1000); // Konversi ke milidetik
+
+// // Format waktu ke zona waktu GMT+7 (Waktu Indonesia Barat)
+// const options = {
+//   timeZone: "Asia/Jakarta", // GMT+7
+//   year: "numeric",
+//   month: "long",
+//   day: "numeric",
+//   hour: "2-digit",
+//   minute: "2-digit",
+//   second: "2-digit",
+// };
+
+// const dateInGMT7 = new Intl.DateTimeFormat("id-ID", options).format(dateUTC);
+app();
+export { app };
